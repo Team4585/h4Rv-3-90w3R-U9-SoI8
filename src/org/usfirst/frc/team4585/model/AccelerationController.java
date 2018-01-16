706906936eb69f9143eeef166064aef8ee53c2a8
cabobalot <casey8cockrell@gmail.com>
2018-01-16 10:04:23 -0700

    uhh...

--- src/org/usfirst/frc/team4585/model/AccelerationController.java
+++ src/org/usfirst/frc/team4585/model/AccelerationController.java
@@ -23,7 +23,7 @@ public class AccelerationController {
 		// + (sqrt(3) sqrt(27 d^4 j^12 - 8 a^3 d^3 j^12) + 9 d^2 j^6)^(1/3) / (3^(2/3) j^3)
 		// + s
 		
-		buffer = (2 * a * d * j) / (Math.pow(3, (1 / 3)) * Math.pow(Math.sqrt(3) * Math.sqrt((27 * Math.pow(d, 4) * Math.pow(j, 12)) - (8 * Math.pow(a, 3) * Math.pow(d, 3) * Math.pow(j, 12))) + (9 * Math.pow(d, 2) * Math.pow(j, 6)), 1 / 3));																									
+		buffer = (2 * a * d * j) / (Math.pow(3, (1 / 3)) * Math.pow(Math.sqrt(3) * Math.sqrt((27 * Math.pow(d, 4) * Math.pow(j, 12)) - (8 * Math.pow(a, 3) * Math.pow(d, 3) * Math.pow(j, 12))) + (9 * Math.pow(d, 2) * Math.pow(j, 6)), 1 / 3));
 		buffer += Math.pow(Math.sqrt(3) * Math.sqrt((27 * Math.pow(d, 4) * Math.pow(j, 12)) - (8 * Math.pow(a, 3) * Math.pow(d, 3) * Math.pow(j, 12))) + (9 * Math.pow(d, 2) * Math.pow(j, 6)), 1 / 3) / (Math.pow(3, 2 / 3) * Math.pow(j, 3));
 		buffer += i;
 		

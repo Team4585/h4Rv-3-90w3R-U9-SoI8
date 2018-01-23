5e2228bb1e14d72590c425633752a4950145ffc4
mewsiof <mewsiof@gmail.com>
2018-01-23 17:39:26 -0500

    Add files via upload

---
+++ Arm.java
@@ -0,0 +1,57 @@
+package org.usfirst.frc.team4585.model;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Arm implements HuskyClass {
+	private final int CLAW_PORT = 6;
+	
+	private Spark arm = new Spark(CLAW_PORT);
+	
+	private Joystick joy;
+	
+	
+	public Arm(Joystick J) {
+		joy = J;
+	}
+	
+	@Override
+	public void teleopInit() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doTeleop() {
+		
+		SmartDashboard.putNumber("Axis 3:", joy.getRawAxis(3));
+		
+		if (joy.getPOV(0) == 0.0) {
+			//claw.set(((-joy.getRawAxis(3) + 1) / 4) + 0.5);
+			
+			arm.set(((-joy.getRawAxis(3) + 1) / 4) + 0.5);
+		}
+		if (joy.getPOV(0) == 180.0) {
+			
+			arm.set( - (((-joy.getRawAxis(3) + 1) / 4) + 0.5));
+		}
+		if (joy.getPOV(0) == -1.0) {
+			arm.set(0);
+		}
+
+	}
+
+	@Override
+	public void autoInit() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doAuto() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Claw.java
@@ -0,0 +1,56 @@
+package org.usfirst.frc.team4585.model;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Claw implements HuskyClass {
+	private final int CLAW_PORT = 7;
+	
+	private Spark claw = new Spark(CLAW_PORT);
+	
+	private Joystick joy;
+	
+	
+	public Claw(Joystick J) {
+		joy = J;
+	}
+	
+	@Override
+	public void teleopInit() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doTeleop() {
+		
+		SmartDashboard.putNumber("Axis 3:", joy.getRawAxis(3));
+		
+		if (joy.getRawButton(5) == true && joy.getRawButton(3) == false) {
+			claw.set(0.5);
+		}
+		if (joy.getRawButton(5) == false && joy.getRawButton(3) == true) {
+			claw.set(-0.5);
+		}
+		if (joy.getRawButton(5) == false && joy.getRawButton(3) == false) {
+			claw.set(0);
+		}
+		if (joy.getRawButton(5) == true && joy.getRawButton(3) == true) {
+			claw.set(0);
+		}
+	}
+
+	@Override
+	public void autoInit() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doAuto() {
+		// TODO Auto-generated method stub
+
+	}
+
+}

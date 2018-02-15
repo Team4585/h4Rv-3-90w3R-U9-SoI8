104fa2de3ac28d11f9993e42a359b3e1ec27a18d
mewsiof <mewsiof@gmail.com>
2018-02-15 16:05:08 -0700

    lifters

---
+++ Robot.java
@@ -0,0 +1,112 @@
+
+package org.usfirst.frc.team4585.robot;
+
+import org.usfirst.frc.team4585.model.*;
+import org.usfirst.frc.team4585.model.auto.GhostController;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Timer;
+
+
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+
+public class Robot extends IterativeRobot {
+	
+	private final int JOYSTICK_PORT = 0;
+	
+	
+	private Timer timer = new Timer();
+	private Joystick joy = new Joystick(JOYSTICK_PORT);
+	private Chassis chassis = new Chassis(joy, timer);
+	private Arm arm = new Arm(joy);
+	private Claw claw = new Claw(joy);
+	private PositionTracker tracker = new PositionTracker(timer);
+	private GhostController marcus = new GhostController(chassis, arm, claw);
+//  private ArmExtender actuator = new ArmExtender(joy);
+	private Winch winch = new Winch(joy); 
+	private ArmActuator actuator = new ArmActuator(joy);
+	private Lifters lifters = new Lifters(joy, timer);
+	private Potentiometer potentiometer = new Potentiometer();
+	
+
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+		
+	}
+
+
+	/**
+	 * This function is run once each time the robot enters autonomous mode.
+	 */
+	@Override
+	public void autonomousInit() {
+		marcus.autoInit();
+		chassis.autoInit();
+		arm.autoInit();
+		claw.autoInit();
+		tracker.autoInit();
+		actuator.autoInit();
+		lifters.autoInit();
+		
+		
+		timer.reset();
+		timer.start();
+	}
+
+	/**
+	 * This function is called periodically during autonomous.
+	 */
+	@Override
+	public void autonomousPeriodic() {
+		marcus.doAuto();
+		chassis.doAuto();
+		arm.doAuto();
+		claw.doAuto();
+		tracker.doAuto();
+		
+	}
+
+	/**
+	 * This function is called once each time the robot enters teleoperated mode.
+	 */
+	@Override
+	public void teleopInit() {
+		chassis.teleopInit();
+		arm.teleopInit();
+		claw.teleopInit();
+		tracker.teleopInit();
+		actuator.teleopInit();
+		
+	}
+
+	/**
+	 * This function is called periodically during teleoperated mode.
+	 */
+	@Override
+	public void teleopPeriodic() {
+		chassis.doTeleop();
+		arm.doTeleop();
+		claw.doTeleop();
+		tracker.doTeleop();
+		actuator.doTeleop();
+	}
+
+	/**
+	 * This function is called periodically during test mode.
+	 */
+	@Override
+	public void testPeriodic() {
+	}
+}
